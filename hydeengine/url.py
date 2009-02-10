edfcbc846a966ed2e6f6c2baa166becd51b24332
Josh Rosen <rosenville@gmail.com>
2009-02-09 18:19:13 -0800

    .lstrip("/").rstrip("/") to .strip("/") in urls.py

--- hydeengine/url.py
+++ hydeengine/url.py
@@ -2,7 +2,7 @@ def join(parent, child):
     return parent.rstrip("/") + "/" + child.lstrip("/")
     
 def fixslash(url, relative=True):
-    url = url.lstrip("/").rstrip("/")
+    url = url.strip("/")
     if relative:
         url = "/" + url
     return url
